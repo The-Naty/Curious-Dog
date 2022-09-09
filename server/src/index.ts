67f83acfc63ff8a266ee92f623622b31d8e69abe
Mosta Eltazy <mostafa.eltazy@levelset.com>
2022-09-10 00:11:06 +0200

    INFRA: Prisma ORM test

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,6 +10,10 @@ server.listen(process.env.PORT, () => {
 const testConnection = (async (): Promise<any> => {
   try {
         const con = await client.connect();
+        // test query to insure db connection
+        // const sql = "SELECT * FROM users where id=1";
+        // const result = await con.query(sql);
+        // console.log(result.rows)
         con.release();
         console.log("connection esablished")
       } catch (err) {
