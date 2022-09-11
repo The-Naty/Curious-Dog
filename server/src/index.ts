d72a75cc2fbc4719047e77bce1b3cb31fe3b74a7
The-Naty <youssef-el-zanaty@outlook.com>
2022-09-11 19:56:55 +0200

    maintain: delete unnecessary testing scripts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,6 @@
 import "dotenv/config";
 import server from "./server";
-import { prisma } from "./database";
 
 server.listen(process.env.PORT, () => {
   console.log("server is running");
 });
-
-const testConnection = (async (): Promise<any> => {
-  try {
-    const users = await prisma.user.findMany();
-
-    console.log(users, "aa");
-
-    console.log("connection esablished");
-  } catch (err) {
-    throw new Error(`Failed to connect due to ${err}`);
-  }
-})();
