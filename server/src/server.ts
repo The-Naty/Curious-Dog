a5ec4f76251e35b4d48fa6412a8d359d0fe463c7
The-Naty <youssef-el-zanaty@outlook.com>
2022-09-15 20:48:10 +0200

    changing files structure

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,14 +3,10 @@ import "./common/utils/handleRunErrors";
 import App from "./app";
 
 import IndexRoute from "./api/routes/index.route";
-import UsersRoute from "./api/routes/users.route";
-
-import validateEnv from "./common/utils/validateEnv";
+// import UsersRoute from "./api/routes/user.route";
 
 (async () => {
-  validateEnv();
-
-  const app = new App([new IndexRoute(), new UsersRoute(), new AuthRoute()]);
-  await app.initializeApp();
-  app.listen();
+  //   const app = new App([new IndexRoute(), new UsersRoute(), new AuthRoute()]);
+  //   await app.initializeApp();
+  //   app.listen();
 })();
