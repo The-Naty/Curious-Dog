864f7367a960722cb4de557e81472011604ebe5b
Mosta Eltazy <mostafa.eltazy@levelset.com>
2022-09-19 01:52:43 +0200

    auth-route:second-check: Implement hash service for user password

--- server/src/services/auth.service.ts
+++ server/src/services/auth.service.ts
@@ -1,18 +1,23 @@
 import { User } from "@prisma/client";
 import { prisma } from "../database";
+import { HashService, IHashService } from './hash.service';
 
 export interface IAuthService {
   registerUser(userData: Partial<User>): Promise<User>;
 }
 class AuthService implements IAuthService{
-  
+
+  constructor(private  hashService: IHashService = new HashService()) {
+  }
+
   public async registerUser(userData: Partial<User>): Promise<User> {
     const { email, password, username, profilePicture } = userData;
+    const hashedPassword = await this.hashService.hashPassword((password) as string)
       const newUser = await prisma.user.create({
           data: {
             email,
             username,
-            password,
+            password: hashedPassword,
             profilePicture
           } as User,
         })    
