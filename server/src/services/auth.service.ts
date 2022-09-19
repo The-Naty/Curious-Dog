72d5953d5568ee3cc8cc7b27c59da18e22ec1aaa
Mosta Eltazy <mostafa.eltazy@levelset.com>
2022-09-19 02:10:53 +0200

    auth-route:seconde-check: addressing review comments

--- server/src/services/auth.service.ts
+++ server/src/services/auth.service.ts
@@ -1,18 +1,18 @@
 import { User } from "@prisma/client";
 import { prisma } from "../database";
-import { HashService, IHashService } from './hash.service';
+import bcrypt from 'bcrypt'
+
 
 export interface IAuthService {
   registerUser(userData: Partial<User>): Promise<User>;
+  
 }
 class AuthService implements IAuthService{
 
-  constructor(private  hashService: IHashService = new HashService()) {
-  }
-
+  
   public async registerUser(userData: Partial<User>): Promise<User> {
     const { email, password, username, profilePicture } = userData;
-    const hashedPassword = await this.hashService.hashPassword((password) as string)
+    const hashedPassword = await this.hashPassword((password) as string)
       const newUser = await prisma.user.create({
           data: {
             email,
@@ -23,6 +23,13 @@ class AuthService implements IAuthService{
         })    
   
       return newUser
+  }
+  private async hashPassword(password: string): Promise<string> {
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    return hashedPassword;
   }
 }
 
