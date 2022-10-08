86265ed7c211c3954f95f2140ebb16d25f85c37f
The-Naty <youssef-el-zanaty@outlook.com>
2022-10-08 06:42:48 +0200

    error-handling: adding middleware

---
+++ server/src/common/errors.ts
@@ -0,0 +1,2 @@
+export class EmailVerificationError extends Error {}
+export class InvalidCredentialsError extends Error {}
---
+++ server/src/middleware/error-handler.middleware.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from 'express';
+import Boom, { isBoom } from '@hapi/boom';
+import { NotFoundError } from '@prisma/client/runtime';
+import { EmailVerificationError, InvalidCredentialsError } from '../common/errors';
+
+const mapError = (error: Error) => {
+  if (error instanceof NotFoundError) {
+    return Boom.notFound(error.message);
+  }
+
+  if (error instanceof EmailVerificationError || error instanceof InvalidCredentialsError) {
+    return Boom.unauthorized(error.message);
+  }
+
+  return error;
+};
+
+export const errorMiddleware = async (error: Boom.Boom | Error, _req: Request, res: Response, _next: NextFunction) => {
+  const mappedError = mapError(error);
+  const statusCode = isBoom(mappedError) ? mappedError.output.statusCode : 500;
+  const errorMessage: string = mappedError.message ?? 'Something went wrong';
+
+  return res.status(statusCode).send(errorMessage);
+};
