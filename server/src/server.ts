6fc8b65f3f45bf52015bc516b831e51f1a88faa1
The-Naty <youssef-el-zanaty@outlook.com>
2022-10-28 21:33:06 +0200

    questions-route: renamed question.route file to user.route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,7 @@
 import App from './app';
 import { IndexRoute } from './api/routes/index.route';
 import { AuthRoute } from './api/routes/auth.route';
-import { QuestionRoute } from './api/routes/question.route';
+import { QuestionRoute } from './api/routes/user.route';
 
 (async () => {
   const app = new App([new IndexRoute(), new AuthRoute(), new QuestionRoute()]);
