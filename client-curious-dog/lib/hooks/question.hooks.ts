2ae1cd08301c93ca325b8092d8f582d7ffa6389a
Mosta Eltazy <mostafa.eltazy@levelset.com>
2023-01-02 21:22:03 +0200

    feedPage  :remove the execute function

--- client-curious-dog/lib/hooks/question.hooks.ts
+++ client-curious-dog/lib/hooks/question.hooks.ts
@@ -1,9 +1,5 @@
 import { useQuery } from 'react-query';
 import { fetchUserQuestions } from '../api/user.api';
 
-function execute(params?: { asked: boolean; limit: number; page: number }) {
-  return fetchUserQuestions(params);
-}
-
 export const useQuestions = (params?: { asked: boolean; limit: number; page: number }) =>
-  useQuery(['user', 'questions', params], () => execute(params), { cacheTime: 0 });
+  useQuery(['user', 'questions', params], () => fetchUserQuestions(params), { cacheTime: 0 });
