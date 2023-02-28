9aa4b92df9e6bd01637a8c433f19781c1ef44517
Mostafa Eltazy <mostafa.eltazy@gmail.com>
2023-03-01 00:17:07 +0200

    center Generic loading

--- client-curious-dog/components/shared-components/GenericLoading.tsx
+++ client-curious-dog/components/shared-components/GenericLoading.tsx
@@ -4,7 +4,7 @@ import { SiDatadog } from 'react-icons/si';
 const GenericLoading = () => {
   return (
     <div className="text-black bg-white" style={{ height: '100vh' }}>
-      <div className="flex justify-center py-24">
+      <div className="flex justify-center items-center py-24 h-full">
         <SiDatadog className="pulsate-fwd" style={{ fontSize: '250px' }} />
       </div>
     </div>
